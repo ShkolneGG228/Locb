5f8dcbdaae84c74cd9f0daa6752b57c1303bd795
ShkolneGG228 <38535930+ShkolneGG228@users.noreply.github.com>
2019-01-03 04:14:07 +0300

    Add files via upload

---
+++ Third/Third/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] st = new Student[10];
+            st[0] = new Student("Иванов", "физ-мат",21,5171,"есть долги");
+            st[1] = new Student("Петров", "филолог", 22, 5141, "сдал");
+            st[2] = new Student("Сидоров", "физ-мат", 21, 1488, "есть долги");
+            st[3] = new Student("Дабдабдабович", "мех-мат", 20, 1355, "олимпиадник");
+            for(int i = 4; i < 10; i++)
+            {
+                st[i] = new Student();
+            }
+            int length = 3;
+            string key, key2, s;
+            int n;
+            do
+            {
+                Console.WriteLine("1 - Добавить нового студента");
+                Console.WriteLine("2 - Удалить студента по номеру зачетки");
+                Console.WriteLine("3 - Редактировать информацию по номеру зачетки");
+                Console.WriteLine("4 - Вывод всех студентов");
+                Console.WriteLine("5 - Поиск информации о студентах по факультету");
+                Console.WriteLine("8 - Выход");
+                key = Console.ReadLine();
+
+                switch (key)
+                {
+                    case "1":
+                        {
+                            do
+                            {
+                                length++;
+                                st[length].AddStud();
+                                Console.WriteLine("Добавить еще(+/-)");
+                                key2 = Console.ReadLine();
+
+                            } while (key2 == "+");
+                        }break;
+
+                    case "2":
+                        {
+                            Console.WriteLine("Введите номер зачетки:");
+                            n = Convert.ToInt32(Console.ReadLine());
+                            int numrem = 0;
+                            bool check = false;
+                            for(int i = 0; i <= length; i++)
+                            {
+                                if(st[i].Number == n) { numrem = i;check = true; }
+                            }
+                            if (check)
+                            {
+                                List<Student> hopa1488 = new List<Student>(st);
+                                hopa1488.RemoveAt(numrem);
+                                st = hopa1488.ToArray();
+                                length--;
+                            }
+                        }break;
+
+                    case "3":
+                        {
+                            Console.WriteLine("Введите номер зачетки: ");
+                            n = Convert.ToInt32(Console.ReadLine());
+                            int numrem=0;
+                            bool check = false;
+                            for (int i = 0; i <= length; i++)
+                            {
+                                if (st[i].Number == n) { numrem = i; check = true; }
+                            }
+                            if (check) { st[numrem].Edit(); }
+                        }
+                        break;
+
+                    case "4":
+                        {
+                            for(int i = 0; i <=length; i++)
+                            {
+                                st[i].Show();
+                            }
+                        }break;
+
+                    case "5":
+                        {
+                            Console.WriteLine("Введите название факультета:");
+                            s = Console.ReadLine();
+                            for(int i = 0; i <= length; i++)
+                            {
+                                if(st[i].Group == s) { st[i].Show(); }
+                            }
+                        }break;
+                }
+
+            } while (key != "8");
+        }
+    }
+}
---
+++ Third/Third/Student.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Third
+{
+    class Student
+    {
+        string fam,group,etc;//фамилия,факультет,индивидуальная инф
+        int age, number;//возраст,номер зачетки
+
+        public Student(string Fam,string Group,int Age,int Number,string Etc)
+        {
+            fam = Fam;group = Group;etc = Etc;age = Age;number = Number;
+        }
+        public Student() { }
+
+        public string Fam
+        {
+            set { fam = value; }
+            get { return fam; }
+        }
+        public string Group
+        {
+            set { group = value; }
+            get { return group; }
+        }
+        public string Etc
+        {
+            set { etc = value; }
+            get { return etc; }
+        }
+        public int Age
+        {
+            set { age = value; }
+            get { return age; }
+        }
+        public int Number
+        {
+            set { number = value; }
+            get { return number; }
+        }
+        string s,subkey;
+        public void AddStud()
+        {
+            Console.WriteLine("Фамилия:");
+            s = Console.ReadLine();Fam = s;
+            Console.WriteLine("Факультет:");
+            s = Console.ReadLine(); Group = s;
+            Console.WriteLine("Возраст:");
+            s = Console.ReadLine(); Age = int.Parse(s);
+            Console.WriteLine("Номер зачетки:");
+            s = Console.ReadLine(); Number = int.Parse(s);
+            Console.WriteLine("Нужно добавить доп информацию о студенте(+/-)");
+            subkey = Console.ReadLine();
+            if(subkey == "+") { s = Console.ReadLine();Etc = s; }
+            else { Etc = "Отсутствует"; }
+        }
+
+        public void Edit()
+        {
+            Console.WriteLine("Что редактировать?\n1 - фамилию\n2 - факультет\n3 - возраст\n4 - номер зачетки\n5 - доп инф о студенте");
+            s = Console.ReadLine();
+            switch (s)
+            {
+                case "1":
+                    {
+                        Console.WriteLine("Новая фамилия:");
+                        Fam = Console.ReadLine();
+                    }break;
+
+                case "2":
+                    {
+                        Console.WriteLine("Новый факультет:");
+                        Group = Console.ReadLine();
+                    }
+                    break;
+
+                case "3":
+                    {
+                        Console.WriteLine("Возраст: ");
+                        Age = int.Parse(Console.ReadLine());
+                    }
+                    break;
+
+                case "4":
+                    {
+                        Console.WriteLine("Номер зачетки: ");
+                        Number = int.Parse(Console.ReadLine());
+                    }
+                    break;
+                case "5":
+                    {
+                        Console.WriteLine("Доп информация: ");
+                        Etc = Console.ReadLine();
+                    }
+                    break;
+
+            }
+
+        }
+
+
+        public void Show()
+        {
+            Console.WriteLine("\nФамилия: {0}", Fam);
+            Console.WriteLine("Факультет: {0},{1} лет", Group, Age);
+            Console.WriteLine("Зачетка: {0}", Number);
+            Console.WriteLine("Доп информация:\n{0}\n", Etc);
+            Console.WriteLine("-------------------------------------------");
+        }
+    }
+}
